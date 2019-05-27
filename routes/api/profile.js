caa7aab4a324d07e23515ea6afa422e8fd583c63
minebox123 <solodeev.09@mail.com>
2019-05-27 22:23:16 +0300

    Create profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load profile model
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -25,6 +28,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -43,51 +47,69 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
-    if (req.body.handle) {
-      return (profileFields.handle = req.body.handle);
-    }
-    if (req.body.company) {
-      return (profileFields.company = req.body.company);
-    }
-    if (req.body.website) {
-      return (profileFields.website = req.body.website);
-    }
-    if (req.body.location) {
-      return (profileFields.location = req.body.location);
-    }
-    if (req.body.bio) {
-      return (profileFields.bio = req.body.bio);
-    }
-    if (req.body.status) {
-      return (profileFields.status = req.body.status);
-    }
-    if (req.body.githubUserName) {
-      return (profileFields.githubUserName = req.body.githubUserName);
-    }
-    // Skills - Split into array
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Skills - Spilt into array
     if (typeof req.body.skills !== "undefined") {
-      profileFields = req.body.skills.split(",");
+      profileFields.skills = req.body.skills.split(",");
     }
-    // Socila
+    // Socials
     profileFields.social = {};
     if (req.body.youtube) {
-      return (profileFields.sociel.youtube = req.body.youtube);
+      profileFields.social.youtube = req.body.youtube;
     }
     if (req.body.twitter) {
-      return (profileFields.sociel.twitter = req.body.twitter);
+      profileFields.social.twitter = req.body.twitter;
     }
     if (req.body.linkedin) {
-      return (profileFields.sociel.linkedin = req.body.linkedin);
+      profileFields.social.linkedin = req.body.linkedin;
     }
     if (req.body.facebook) {
-      return (profileFields.sociel.facebook = req.body.facebook);
+      profileFields.social.facebook = req.body.facebook;
     }
     if (req.body.instagram) {
-      return (profileFields.sociel.instagram = req.body.instagram);
+      profileFields.social.instagram = req.body.instagram;
     }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
+
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+
+          // Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
   }
 );
 
