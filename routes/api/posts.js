0087275709db6e410df24408829052f2bffb0e1a
minebox123 <solodeev.09@mail.com>
2019-05-28 15:11:15 +0300

    Add post option

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Post model
+const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
 
 // @route GET api/posts/test
 // @desc Tests posts route
@@ -9,5 +17,31 @@ router.get("/test", (req, res) => {
     msg: "Posts Work"
   });
 });
+
+// @route POST api/posts
+// @desc Create post
+// @access Privat
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
