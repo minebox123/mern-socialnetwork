f2dbd61deb69fcb75353d33f9a4f8685ab9f554a
minebox123 <solodeev.09@mail.com>
2019-06-03 15:48:16 +0300

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Ilya:Ilya@mern-app-zeomb.mongodb.net/test?retryWrites=true",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
